bd225a594fbf538b4a8f8cc899a6ad7a9a30648a
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-01-20 21:29:29 +0900

    transformerの構造の変更

--- learnning_model/Transformer.py
+++ learnning_model/Transformer.py
@@ -28,48 +28,81 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+class Embedder(nn.Module):
+    def __init__(self):
+        super(Embedder, self).__init__()
+        self.embeddings = nn.Embedding.from_pretrained(
+            embeddings=TEXT.vocab.vectors, freeze=True)
+
+    def forward(self, x):
+        x_vec = self.embeddings(x)
+        return x_vec.to(device)
+
+
 class Transformer(nn.Module):
     def __init__(self, d_model: int = 300, nhead: int = 6, num_encoder_layers: int = 4,
                  num_decoder_layers: int = 4, dim_feedforward: int = 1024, dropout: float = 0.1,
-                 activation: str = "relu", words_num: int = 0) -> None:
+                 activation: str = "relu", target_vocab_length: int = TEXT.vocab.vectors.shape[0]) -> None:
         super(Transformer, self).__init__()
 
-        self.source_embedding = nn.Embedding(words_num, 300)
+        self.source_embedding = nn.Embedding(target_vocab_length, 300)
+        #self.source_embedding = Embedder().cuda()
         self.pos_encoder = PositionalEncoding(
-            d_model=d_model, dropout=dropout, max_len=75)
+            d_model=d_model, dropout=dropout, max_len=MAX_SEQ_LEN)
         self.pos_decoder = PositionalEncoding(
-            d_model=d_model, dropout=dropout, max_len=74)
+            d_model=d_model, dropout=dropout, max_len=MAX_SEQ_LEN)
         encoder_layer = nn.TransformerEncoderLayer(
             d_model, nhead, dim_feedforward, dropout, activation)
         encoder_norm = nn.LayerNorm(d_model)
         self.encoder = nn.TransformerEncoder(
             encoder_layer, num_encoder_layers, encoder_norm)
 
-        self.target_embedding = nn.Embedding(words_num, 300)
+        self.target_embedding = nn.Embedding(target_vocab_length, 300)
+        #self.target_embedding = Embedder().cuda()
         decoder_layer = nn.TransformerDecoderLayer(
             d_model, nhead, dim_feedforward, dropout, activation)
         decoder_norm = nn.LayerNorm(d_model)
         self.decoder = nn.TransformerDecoder(
             decoder_layer, num_decoder_layers, decoder_norm)
-        self.out = nn.Linear(d_model, words_num)
+        self.out = nn.Linear(d_model, target_vocab_length)
         self._reset_parameters()
         self.d_model = d_model
         self.nhead = nhead
+
+        self.src_key_padding_mask = None
+        self.tgt_key_padding_mask = None
+
+    def _generate_square_subsequent_mask(self, src):
+        input_pad = TEXT.vocab.stoi['<pad>']
+        mask = (src == input_pad)
+        return mask
 
     def forward(self, src: Tensor, tgt: Tensor, src_mask: Optional[Tensor] = None, tgt_mask: Optional[Tensor] = None,
                 memory_mask: Optional[Tensor] = None, src_key_padding_mask: Optional[Tensor] = None,
                 tgt_key_padding_mask: Optional[Tensor] = None, memory_key_padding_mask: Optional[Tensor] = None) -> Tensor:
         if src.size(1) != tgt.size(1):
             raise RuntimeError("the batch number of src and tgt must be equal")
+
+        if self.src_key_padding_mask is None or self.src_key_padding_mask.size(0) != len(src):
+            device = src.device
+            mask = self._generate_square_subsequent_mask(src).to(device)
+            self.src_key_padding_mask = mask.transpose(0, 1)
+
+        if self.tgt_key_padding_mask is None or self.tgt_key_padding_mask.size(0) != len(src):
+            device = tgt.device
+            mask = self._generate_square_subsequent_mask(tgt).to(device)
+            self.tgt_key_padding_mask = mask.transpose(0, 1)
+
         src = self.source_embedding(src)
         src = self.pos_encoder(src)
-        memory = self.encoder(src, mask=src_mask,
-                              src_key_padding_mask=src_key_padding_mask)
+
         tgt = self.target_embedding(tgt)
         tgt = self.pos_decoder(tgt)
+
+        # , src_key_padding_mask=self.src_key_padding_mask)
+        memory = self.encoder(src)
         output = self.decoder(tgt, memory, tgt_mask=tgt_mask, memory_mask=memory_mask,
-                              tgt_key_padding_mask=tgt_key_padding_mask,
-                              memory_key_padding_mask=memory_key_padding_mask)
+                              memory_key_padding_mask=memory_key_padding_mask)  # ,tgt_key_padding_mask=self.tgt_key_padding_mask)
         output = self.out(output)
         return output
 
